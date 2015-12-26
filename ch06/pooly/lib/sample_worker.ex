c8f6c4ae9b881638267a0cee7e591a26347f6102
Sean Yu <seansu4you87@gmail.com>
2015-12-26 18:45:36 -0500

    Chapter 6

---
+++ ch06/pooly/lib/sample_worker.ex
@@ -0,0 +1,23 @@
+defmodule SampleWorker do
+  use GenServer
+
+  #######
+  # API #
+  #######
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def stop(pid) do
+    GenServer.call(pid, :stop)
+  end
+
+  #############
+  # CALLBACKS #
+  #############
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
+  end
+end
